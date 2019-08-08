eade9707d0021fee8096c9363599580836fb7faf
Patrick Wang <patrick@covar.com>
2019-08-08 16:52:59 -0400

    test OpenAPI 3.0 compliance

---
+++ tests/test_valid.py
@@ -0,0 +1,19 @@
+"""OpenAPI 3.0 validation."""
+import json
+import os
+
+import jsonschema
+import requests
+import yaml
+
+
+def test_valid():
+    """Test whether TranslatorReasonersAPI.yaml is valid OpenAPI 3.0."""
+    response = requests.get('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v3.0/schema.json')
+    openapi_schema = json.loads(response.text)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, '..', 'API', 'TranslatorReasonersAPI.yaml')) as f:
+        spec = yaml.load(f, Loader=yaml.SafeLoader)
+
+    jsonschema.validate(spec, openapi_schema)
