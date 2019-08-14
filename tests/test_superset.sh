53e31dbe461a2471154f834fdb66cc562feb54c2
Patrick Wang <patrick@covar.com>
2019-08-14 12:49:57 -0400

    add helpful test output

--- tests/test_superset.sh
+++ tests/test_superset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-STATS=$(
-    diff API/TranslatorReasonersAPI.yaml API/TranslatorReasonersAPI_optional.yaml \
-    | \
-    diffstat
-)
-if grep -q "deletion" <<< "$STATS"; then
+DIFF=$(diff API/TranslatorReasonersAPI.yaml API/TranslatorReasonersAPI_optional.yaml)
+STATS=$(diffstat <<< "$DIFF")
+if echo "$STATS" | grep -q "deletion"; then
+    >&2 echo 'Some lines from API/TranslatorReasonersAPI.yaml do not exist in API/TranslatorReasonersAPI_optional.yaml:'
+    DELS=$(echo "$DIFF" | grep -E '^<' | sed -E 's/^<//')
+    >&2 echo "$DELS"
     exit 1
 else
     exit 0
