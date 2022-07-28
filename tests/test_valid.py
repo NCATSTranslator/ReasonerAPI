0a2dac4b7d21d33df22b331db00c0954ef5a0d8b
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-07-28 16:11:00 -0700

    add complex examples and tests

--- tests/test_valid.py
+++ tests/test_valid.py
@@ -1,7 +1,7 @@
 """OpenAPI 3.0 validation."""
 import json
 import os
-
+from pprint import pprint
 import jsonschema
 import requests
 import yaml
@@ -17,3 +17,20 @@ def test_valid():
         spec = yaml.load(f, Loader=yaml.SafeLoader)
 
     jsonschema.validate(spec, openapi_schema)
+
+
+def test_examples():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, '..', 'TranslatorReasonerAPI.yaml')) as f:
+        spec = yaml.load(f, Loader=yaml.SafeLoader)
+        pprint(spec)
+
+    directory = '../examples/Message'
+
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            with open(f) as f:
+                example = yaml.load(f, Loader=yaml.SafeLoader)
+                jsonschema.validate(example, spec)
